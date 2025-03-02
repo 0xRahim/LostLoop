89d23f1b840749898e6ca1f248631a2ec122c2eb
zenithx01 <rahimudiin18@gmail.com>
2025-03-02 18:21:39 +0530

    changed api url

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "https://lostloop.onrender.com/";
 let postIdToDelete = null;
 // Helper: Retrieve token from localStorage
 function getToken() {
